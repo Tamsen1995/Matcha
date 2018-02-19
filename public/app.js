54be3caa4620a42310e6741852fa2300f238c875
Tamsen1995 <Tamanhbui95@gmail.com>
2018-02-19 18:15:19 +0100

    [UPD] Login route added

--- public/app.js
+++ public/app.js
@@ -6,12 +6,26 @@ CreateUser.addEventListener('submit', (e) => {
 	post('/createUser', { username, password });
 });
 
+
+const Login = document.querySelector('.Login')
+Login.addEventListener('submit', (e) => {
+	e.preventDefault()
+	const username = Login.querySelector('.username').value
+	const password = Login.querySelector('.password').value
+	post('/login', { username, password })
+		.then(({ status }) => {
+			if (status === 200) alert('login success')
+			else alert('login failed')
+		})
+})
+
 function post(url, data) {
 	return fetch(url, {
 		body: JSON.stringify(data),
 		cache: 'no-cache',
 		credentials: 'same-origin',
 		headers: {
+			'Accept': 'application/json',
 			'content-type': 'application/json',
 		},
 		method: 'POST',
